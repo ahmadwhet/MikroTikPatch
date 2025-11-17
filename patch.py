972c8a2ec96b383f67899980bbdc4a741c1efeca
WCKD <36060669+madskid@users.noreply.github.com>
2025-11-17 19:17:03 +0700

    Update patch.py
    Signed-off-by: WCKD <36060669+madskid@users.noreply.github.com>

--- patch.py
+++ patch.py
@@ -388,7 +388,7 @@ def patch_npk_package(package, key_dict):
 #        run_shell_command(f"sudo sed -i '8s#.*#  Ludens  		    https://github.com/madskid/MikrotikPatch#' {logo}")
         logo_src = os.path.join(os.getcwd(), "logo.txt")
         logo_dst = os.path.join(extract_dir, "nova/lib/console/logo.txt")
-        if os.path.exists(loader_src):
+        if os.path.exists(logo_src):
             run_shell_command(f"cp {logo_src} {logo_dst}")
             run_shell_command(f"chmod 755 {logo_dst}")
             print(f"copied loader -> {logo_dst}")
@@ -467,6 +467,7 @@ if __name__ == '__main__':
 
 
     
+
 
 
 
